554c3ef6f80bb2c583514891f73187c3441f07fb
DanRuizK57 <d.ruizk03@ufromail.cl>
2023-11-03 18:18:18 -0300

    Header añadido

--- lib/src/pages/products_page.dart
+++ lib/src/pages/products_page.dart
@@ -8,11 +8,59 @@ class ProductsPage extends StatefulWidget {
 }
 
 class _HomePageState extends State<ProductsPage> {
+  String _cafeteriaName = "Casino Los Notros";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Lista de Productos"),
+          leading: IconButton(
+            icon: const Icon(Icons.list_outlined),
+            onPressed: () => {},
+          ),
+          backgroundColor: Colors.white,
+          iconTheme:
+              const IconThemeData(color: Color.fromARGB(255, 97, 97, 97)),
+          actions: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 8.0),
+              child: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  // Acción cuando se presiona el icono derecho
+                },
+              ),
+            ),
+          ]),
+      body: Column(
+        children: [
+          _titulo(),
+          _subtitulo(),
+        ],
+      ),
+    );
+  }
+
+  Widget _titulo() {
+    return Container(
+      margin: const EdgeInsetsDirectional.only(top: 20.0, start: 20.0),
+      child: const Text(
+        "Productos",
+        style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 97, 97, 97)),
+      ),
+    );
+  }
+
+  Widget _subtitulo() {
+    return Container(
+      margin: const EdgeInsetsDirectional.only(start: 10.0, top: 10.0),
+      child: Text(
+        _cafeteriaName,
+        style: const TextStyle(
+            fontSize: 15.0, color: Color.fromARGB(255, 97, 97, 97)),
       ),
     );
   }
